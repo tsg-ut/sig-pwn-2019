161d1fcf2041532bec12642b7a39739ba95fb496
vagrant <vagrant@ubuntu1904.localdomain>
2019-09-15 01:37:00 -0700

    add timer3

---
+++ timer3.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Satoru Takeuchi <satoru.takeuchi@gmail.com>");
+MODULE_DESCRIPTION("timer kernel module: oneshot timer");
+
+struct timer_list mytimer;
+
+#define MYTIMER_TIMEOUT_SECS    10
+
+static void mytimer_fn(struct timer_list *t)
+{ 
+	printk(KERN_ALERT "10 secs passed.\n");
+	mod_timer(&mytimer,jiffies + 10 * HZ);
+}
+static int mymodule_init(void)
+{
+	timer_setup(&mytimer,mytimer_fn,0);
+	mytimer.expires = jiffies + MYTIMER_TIMEOUT_SECS*HZ;
+	add_timer(&mytimer);
+	return 0;
+}
+
+static void mymodule_exit(void)
+{ 
+	del_timer(&mytimer);
+}
+
+module_init(mymodule_init);
+module_exit(mymodule_exit);
